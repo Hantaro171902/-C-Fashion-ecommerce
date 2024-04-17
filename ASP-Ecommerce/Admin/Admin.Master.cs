0eceb5da5736189613d1616bb1afb6be41dba69d
Hantaro191702 <chaugiahan001@gmail.com>
2024-04-18 00:28:13 +0700

    add admin ashboard

--- ASP-Ecommerce/Admin/Admin.Master.cs
+++ ASP-Ecommerce/Admin/Admin.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace ASP_Ecommerce.Admin
 {
-    public partial class Site1 : System.Web.UI.MasterPage
+    public partial class Admin : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
